f037087eea8c7d609b9a40d3b819c179e88d8ad5
Salvador Marquez <ing.salvadormarquez@hotmail.com>
2024-10-22 16:36:48 -0600

    Adding enumerator for checking types

---
+++ Kumiko.Application/Shared/Enums/CheckTypeEnum.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Kumiko.Application.Shared.Enums;
+
+public enum CheckTypeEnum
+{
+    CheckIn,
+    CheckOut,
+    CheckInLunchTime,
+    CheckOutLunchTime,
+}
--- Kumiko.Domain/Entities/ChecktTime.cs
+++ Kumiko.Domain/Entities/ChecktTime.cs
@@ -1,8 +1,10 @@
-﻿using Kumiko.Domain.Entities.Base;
+﻿using Kumiko.Application.Shared.Enums;
+using Kumiko.Domain.Entities.Base;
 
 namespace Kumiko.Domain.Entities;
 
 public class ChecktTime : ModelBase //quitas interna y pones publica (nivel de acceso)
 {
     public DateTime Check {  get; set; }    
+    public CheckTypeEnum CheckType { get; set; }
 }
